da6d7878311094ba4a8101b812fe09bb55a4406c
Gustavo Prado <144186260+gustavoprado0@users.noreply.github.com>
2023-11-21 19:07:38 -0300

    Add files via upload

---
+++ dia #09/index.js
@@ -0,0 +1,11 @@
+let conta = parseInt(prompt("Insira algum número:"))
+let conta2 = parseInt(prompt("Insira outro número:"))
+
+
+function somar (num1, num2){
+    return num1 + num2;
+}
+let resultadoSoma = somar(conta, conta2);
+console.log(resultadoSoma)
+
+
